88db47d29d0e6cf69935f00eeda9eca532a28caa
Tobias Briones <tobiasbrionesdev@gmail.com>
2019-11-23 17:41:35 -0600

    Initial project structure

---
+++ Src/Main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2019 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
